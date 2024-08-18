27c06ef32b1a253a2313f427cd32bab99a9c1fb9
Harsh Gupta <guptaharsh940@gmail.com>
2024-08-18 23:34:37 +0530

    for vercel swagger

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api-docs', swaggerui.serve, swaggerui.setup(swaggerdoc))
+app.use('/api-docs', swaggerui.serve, swaggerui.setup(swaggerdoc, { customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css" }))
 
 
 // Basic route to check server status
